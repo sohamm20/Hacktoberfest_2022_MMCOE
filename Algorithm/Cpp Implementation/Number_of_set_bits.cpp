14a4738c33da610bf42bb73c3407bfe913abb0a9
Soham Mirikar <82722112+sohamm20@users.noreply.github.com>
2022-10-19 23:20:23 +0530

    Update Number_of_set_bits.cpp

--- Algorithm/Cpp Implementation/Number_of_set_bits.cpp
+++ Algorithm/Cpp Implementation/Number_of_set_bits.cpp
@@ -6,8 +6,16 @@ int main()
     int n;
     cout<<"Enter the number: ";
     cin>>n;
-    cout << "The total number of set bits in " << n << " is "
-         << __builtin_popcount (n) << endl;
+
+    int ans = 0;
+
+    for (int i = 0; i < 32; i++){
+        if (1 << i & n){
+            ans++;
+        }
+    }
+
+    cout << "The total number of set bits in " << n << " is " << ans << endl;
  
     return 0;
 }

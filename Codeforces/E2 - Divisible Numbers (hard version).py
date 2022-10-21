a5c031fa507f4b0eb03c783ae707785837462aa8
dnyanmiri <99322999+dnyanmiri@users.noreply.github.com>
2022-10-21 19:50:15 +0530

    Create E2 - Divisible Numbers (hard version).py

---
+++ Codeforces/E2 - Divisible Numbers (hard version).py
@@ -0,0 +1,152 @@
+# def mi():
+#     return map(int, input().split())
+mi = lambda : map(int, input().split())
+def li():
+    return list(mi())
+def si():
+    return str(input())
+def ni():
+    return int(input())
+
+mi = lambda : map(int, input().split())
+
+"""
+
+possible 1, 2
+
+1 1
+2 1
+1 2
+2 2
+
+
+
+"""
+
+from collections import Counter
+
+
+def gcd(x, y):
+    """greatest common divisor of x and y"""
+    while y:
+        x, y = y, x % y
+    return x
+
+
+def memodict(f):
+    """memoization decorator for a function taking a single argument"""
+    class memodict(dict):
+        def __missing__(self, key):
+            ret = self[key] = f(key)
+            return ret
+
+    return memodict().__getitem__
+
+
+def pollard_rho(n):
+    """returns a random factor of n"""
+    if n & 1 == 0:
+        return 2
+    if n % 3 == 0:
+        return 3
+
+    s = ((n - 1) & (1 - n)).bit_length() - 1
+    d = n >> s
+    for a in [2, 325, 9375, 28178, 450775, 9780504, 1795265022]:
+        p = pow(a, d, n)
+        if p == 1 or p == n - 1 or a % n == 0:
+            continue
+        for _ in range(s):
+            prev = p
+            p = (p * p) % n
+            if p == 1:
+                return gcd(prev - 1, n)
+            if p == n - 1:
+                break
+        else:
+            for i in range(2, n):
+                x, y = i, (i * i + 1) % n
+                f = gcd(abs(x - y), n)
+                while f == 1:
+                    x, y = (x * x + 1) % n, (y * y + 1) % n
+                    y = (y * y + 1) % n
+                    f = gcd(abs(x - y), n)
+                if f != n:
+                    return f
+    return n
+
+
+@memodict
+def prime_factors(n):
+    """returns a Counter of the prime factorization of n"""
+    if n <= 1:
+        return Counter()
+    f = pollard_rho(n)
+    return Counter([n]) if f == n else prime_factors(f) + prime_factors(n // f)
+
+
+def distinct_factors(n):
+    """returns a list of all distinct factors of n"""
+    factors = [1]
+    for p, exp in prime_factors(n).items():
+        factors += [p**i * factor for factor in factors for i in range(1, exp + 1)]
+    return factors
+
+
+def all_factors(n):
+    """returns a sorted list of all distinct factors of n"""
+    small, large = [], []
+    for i in range(1, int(n**0.5) + 1, 2 if n & 1 else 1):
+        if not n % i:
+            small.append(i)
+            large.append(n // i)
+    if small[-1] == large[-1]:
+        large.pop()
+    large.reverse()
+    small.extend(large)
+    return small
+
+
+from math import gcd
+
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+def find(x, a, b, c, d):
+    y = lcm(a * b, x) // x
+    target = y * (int(d // y))
+    return target
+    # if target <= d and target > b:
+    #     # ans = int(x)
+    #     # temp = int(target)
+    #     return target
+
+for _ in range(int(input())):
+
+    a, b, c, d = map(int, input().split())
+
+    f = all_factors(a)
+    g = all_factors(b)
+
+    h = []
+
+    for i in f:
+        for j in g:
+            temp = i*j
+            h.append(temp)
+
+    ans = -1
+    temp = -1
+    for x in h:
+        # y = lcm(a * b, x) // x
+        y = a*b // x
+        target = y * (int(d // y))
+        if target <= d and target > b:
+            temp1 = find(target, b, a, d, c)
+            # temp = int(target)
+            if temp1 > a and temp1 <= c:
+                ans = int(temp1)
+                temp = int(target)
+                break
+    print(ans, temp)

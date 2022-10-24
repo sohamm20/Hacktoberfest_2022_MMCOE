d20a61ee2dc0481c35024266abee86cdf1878c40
Rushikesh Adhav <adhavrushikesh6@gmail.com>
2022-10-25 00:33:35 +0530

    Made some changes in the implementation

--- Algorithm/Cpp Implementation/selection_sort.cpp
+++ Algorithm/Cpp Implementation/selection_sort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int *xp, int *yp)
+void swapElements(int *xp, int *yp)
 {
     int temp = *xp;
     *xp = *yp;
@@ -10,24 +10,22 @@ void swap(int *xp, int *yp)
 
 void selectionSort(int arr[], int n)
 {
-    int i, j, min_idx;
+    int min;
 
-    // One by one move boundary of
-    // unsorted subarray
-    for (i = 0; i < n - 1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
 
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
+        min = i;
+        for (int j = i + 1; j < n; j++)
+            if (arr[j] < arr[min])
+                min = j;
 
-        if (min_idx != i)
-            swap(&arr[min_idx], &arr[i]);
+        if (min != i)
+            swapElements(&arr[min], &arr[i]);
     }
 }
 
-void printArray(int arr[], int size)
+void printSortedArray(int arr[], int size)
 {
     int i;
     for (i = 0; i < size; i++)
@@ -39,30 +37,29 @@ int main()
 {
     int size;
 
-    cout << "Enter the size of array:- ";
+    cout << "Enter the size: ";
     cin >> size;
 
     int arr[size];
 
-    cout << "Enter the elements of array:- ";
+    cout << "Enter the elements: ";
 
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
-    cout << "Before Sorting it looked like:- ";
+    cout << "Before Sorting: ";
 
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << "\t";
     }
 
-    selectionSort(arr, size); // sorting
+    selectionSort(arr, size);
 
     cout << endl;
 
-    cout << endl
-         << "After Sorting it looks like:- ";
+    cout << "After Sorting: ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << "\t";
